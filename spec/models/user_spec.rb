dcc16bbf976be6b515294fb209a8bd9c8389334f
lllverb <wktkm9orz2@gmail.com>
2019-10-14 13:11:00 +0900

    日本語化

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,47 +1,47 @@
 require 'rails_helper'
 describe User do
   describe '#create' do 
-    it "is valid with a nickname, email, password, password_confirmation" do
+    it "ニックネーム, メアド, パスワード, 確認があれば通る" do
       user = build(:user)
       user.valid?
       expect(user).to be_valid
     end
-    it "is invalid without a nickname" do
+    it "ニックネーム無しでは通らない" do
       user = build(:user, nickname: "")
       user.valid?
       expect(user.errors[:nickname]).to include("can't be blank")
     end
-    it "is invalid without a email" do
+    it "メアド無しでは通らない" do
       user = build(:user, email: "")
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
-    it "is invalid without a password" do
+    it "パスワード無しでは通らない" do
       user = build(:user, password: "")
       user.valid?
       expect(user.errors[:password]).to include("can't be blank")
     end
-    it "is invalid without a password_confirmation" do
+    it "確認無しでは通らない" do
       user = build(:user, password_confirmation: "")
       user.valid?
       expect(user.errors[:password_confirmation]).to include("doesn't match Password")
     end
-    it "is invalid with a nickname that has more than 9 characters" do
+    it "ニックネーム9文字以上では通らない" do
       user = build(:user, nickname: "123456789")
       user.valid?
       expect(user.errors[:nickname][0]).to include("is too long")
     end
-    it "is valid with a nickname that has less than 8 characters" do
+    it "ニックネーム8文字なら通る" do
       user = build(:user, nickname: "12345678")
       user.valid?
       expect(user).to be_valid
     end
-    it "is invalid without a password that has less than 5 characters" do
+    it "パスワード5文字以下では通らない" do
       user = build(:user, password: "12345", password_confirmation: "12345")
       user.valid?
       expect(user.errors[:password][0]).to include("is too short")
     end
-    it "is valid with a password that has more than 6 characters" do
+    it "パスワード6文字以上なら通る" do
       user = build(:user, password: "123456", password_confirmation: "123456")
       user.valid?
       expect(user).to be_valid
