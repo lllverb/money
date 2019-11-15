bdf59831861c1c14ab0e9a87d5b6999ded4169a0
lllverb <wktkm9orz2@gmail.com>
2019-11-15 17:34:47 +0900

    optional true

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   has_many :details
+  has_many :howmuches
 end
--- app/models/howmuch.rb
+++ app/models/howmuch.rb
@@ -1,2 +1,5 @@
 class Howmuch < ApplicationRecord
+  belongs_to :user, optional: true
+  belongs_to :category, optional: true
+  belongs_to :detail, optional: true
 end
