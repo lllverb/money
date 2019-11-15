c9f4e790a5226dbf3b7741ab60d3800bb6fc2c8f
lllverb <wktkm9orz2@gmail.com>
2019-11-15 17:42:22 +0900

    migrateエラー解消

--- db/migrate/20190923113205_create_details.rb
+++ db/migrate/20190923113205_create_details.rb
@@ -2,7 +2,6 @@ class CreateDetails < ActiveRecord::Migration[5.2]
   def change
     create_table :details do |t|
       t.string     :name, null: false
-      t.integer    :money, null: false
       t.references :category, null:false, foreign_key: true
       t.timestamps
     end
