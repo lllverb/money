1511870308437fb74bc9a45ed44093f32fc19b24
lllverb <wktkm9orz2@gmail.com>
2019-11-15 16:33:15 +0900

    day　削除

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,12 +1,4 @@
 document.addEventListener("turbolinks:load", function() {
-  // 日付クリック////////////
-  $('#calendar').fullCalendar({
-    defaultDate: $('.theyear').val() + "-" + $('.themonth').val() + "-" + "15",
-    dayClick: function(date, jsEvent, view) {
-      const theday = $(this).data('date').split('-');
-      location.href = '/years/' + theday[0] + '/months/' + theday[1] + '/days/' + theday[2]
-    }
-  });
   // キーボードでカレンダーめくる
   var left  = 0;
   var right = 0;
